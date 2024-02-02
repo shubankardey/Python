bfac9cdde09f46a21fb0badde1e634287a75d34f
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-02-02 15:58:16 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello!")
+      
+name = input("Enter your name:- ")
+age = input("Enter your age:- ")
+gender = input("Enter your gender (M/F):- ")
+weight = input("Enter your weight:- ")
+
+
+print("\nYour result is here:- ")
+
+print(name)
+print(age)
+print(gender)
+print(weight)
