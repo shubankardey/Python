654172ac8ecfe891b6a852f346752f46682dae5b
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-02-02 15:51:55 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+# Write a python program to taking input from user i.e. name,age,gender(single character),weight
+# and display the output of the same.
+
+print("Hello!")
+      
+name = input("Enter your name:- ")
+age = input("Enter your age:- ")
+gender = input("Enter your gender (M/F):- ")
+weight = input("Enter your weight (in kg):- ")
+
+
+print("\nYour output is here:- ")
+
+print("Your name is:", name)
+print("Your age is:", age,"Years")
+print("Your gender is:", gender)
+print("Your weight is:", weight,"Kgs")
