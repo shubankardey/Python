4d6765128b200fda263db49abd82fbcbc9875313
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-02-02 14:52:23 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Write a program to taking input from user i.e. name,age,gender(single character),weight
+# and display the output of the same.
+
+print("Hello!")
+      
+name = input("Enter your name:- ")
+age = input("Enter your age:- ")
+gender = input("Enter your gender M/F:- ")
+weight = input("Enter your weight:- ")
+
+
+print("Your output is here:- ")
+
+print(name)
+print(age,"Years")
+print(gender)
+print(weight,"Kgs")
