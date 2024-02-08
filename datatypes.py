2dd9d9d69d8e0a2c408d2f346c8e20d6fe454bae
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-02-08 16:43:38 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+print("Hello!")
+name=input("Enter your name: ")#string
+age=int(input("Enter your age: "))#string
+gender=input("Enter your gender (M/F): ")#string
+weight=float(input("Enter your weight: "))#string
+
+print("\nYour result is here:- ")
+
+print("your name is:",name,type(name))
+print("Your age is:",age,"years",type(age))
+print("Your gender is:",gender,type(gender))
+print("your weight is:",weight,"kg",type(weight))#55.56
+weight=int(weight)
+print("Your weight is:",weight,"kg",type(weight))
+#float>int
