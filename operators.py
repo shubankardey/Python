376600c718bbad367426e468316b2c704c2c62f0
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-02-13 14:35:20 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,27 @@
+a=20
+b=6
+
+#arithmetic
+print("sum:",a+b)
+print("multiply:",a*b)
+print("exponentiation:",a**b)
+print("floor divison:",a//b)
+print("modulus:",a%b)
+
+#comparison
+print("greater than",a>b)
+print("less than",a<b)
+print("equal",a==b)
+print("not equal",a!=b)
+
+#logical
+x=2
+
+print(x>3 and x<10)
+x=5
+
+print(x>3 and x<4)
+x=5
+
+print(not(x>3 and x<10))
+#exp=a>b and a==b #true and true->true
