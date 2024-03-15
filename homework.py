0be20d76fd1893d2f45e316f5696d78aea26ff61
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-03-16 01:38:17 +0530

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,13 @@
+#string to integer:
+
+string=str(input("Enter a number as string: "))
+print(type(string))
+string=int(string)
+print("converted to string to integer:",string,type(string))
+
+#integer to float:
+
+integer=int(input("Enter a number as integer: "))
+print(type(integer))
+integer=float(integer)
+print("converted to integer to float:",integer,type(integer))
